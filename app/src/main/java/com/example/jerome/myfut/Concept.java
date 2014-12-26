547eb75afdfa09cacf0ba669ec99928aa7a03a88
Gg <jerome.merle.work@gmail.com>
2014-12-26 12:06:49 +0100

    Adding an Action Bar --Link to order menu

--- app/src/main/java/com/example/jerome/myfut/Concept.java
+++ app/src/main/java/com/example/jerome/myfut/Concept.java
@@ -1,6 +1,7 @@
 package com.example.jerome.myfut;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,7 +20,7 @@ public class Concept extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_concept, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -27,13 +28,16 @@ public class Concept extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId())
+        {
+            case R.id.action_order:
+                Intent intent = new Intent(this, Order.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                return true;
+            default :
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
