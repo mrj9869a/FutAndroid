3e2111e6abc23fbf84ec27b5d5c65f7294b31381
Gg <jerome.merle.work@gmail.com>
2014-12-29 12:28:18 +0100

    Activity Contact designed -- + premise of asynctask

--- app/src/main/java/com/example/jerome/myfut/Order.java
+++ app/src/main/java/com/example/jerome/myfut/Order.java
@@ -1,9 +1,20 @@
 package com.example.jerome.myfut;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class Order extends Activity {
@@ -12,8 +23,8 @@ public class Order extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        new DLTask().execute("http://fabrigli.fr/cours/example.json");
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +46,48 @@ public class Order extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DLTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page, URL may be invalid.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            Toast.makeText(getApplication().getApplicationContext(),result,Toast.LENGTH_LONG).show();
+        }
+
+        private String downloadUrl(String myurl) throws IOException {
+            InputStream is = null;
+            try{
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
+                int response = conn.getResponseCode();
+
+                //Convert the InputStream into a string
+                String contentAsString = readIt(is,500);
+                return contentAsString;
+            } finally{
+                if (is !=null){
+                    is.close();
+                }
+            }
+        }
+
+        public String readIt(InputStream stream, int len) throws IOException, UnsupportedOperationException{
+            Reader reader = null;
+            reader = new InputStreamReader(stream,"UTF-8");
+            char[] buffer = new char [len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
     }
 }
--- app/src/main/java/com/example/jerome/myfut/OurBeer.java
+++ app/src/main/java/com/example/jerome/myfut/OurBeer.java
@@ -1,7 +1,6 @@
 package com.example.jerome.myfut;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,16 +27,13 @@ public class OurBeer extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId())
-        {
-            case R.id.action_order:
-                Intent intent = new Intent(this, Order.class);
-                startActivity(intent);
-                return true;
-            case R.id.action_settings:
-                return true;
-            default :
-                return super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
 }
